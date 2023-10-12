f2f92311686c24e089ed9d714b77198d9bad3f74
Muhammad Naseem <alinaseem366@gmail.com>
2023-10-12 23:51:12 +0500

    first commit

--- index.js
+++ index.js
@@ -2,5 +2,8 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-
+const main=()=>
+{
+    console.log("hello")
+}
 AppRegistry.registerComponent(appName, () => App);
